bb48f6e0239057df1110820f35d37badbad6e385
Bryan Luu <bluu@proofgov.com>
2021-08-11 17:36:19 -0700

    Add Date dropdown

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,10 @@
 class FlightsController < ApplicationController
   def index
     @cities = Airport.all.map { |a| a.airport_code }
+    @dates = flight_dates
+  end
+
+  def flight_dates
+    Flight.all.map { |f| f.start.noon.to_datetime.strftime('%d/%m/%Y') }.uniq.sort
   end
 end
