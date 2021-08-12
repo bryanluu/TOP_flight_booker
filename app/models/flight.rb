d2d5740f2d4cc2ceb0021de6a68a6f2476413267
Bryan Luu <bluu@proofgov.com>
2021-08-11 19:37:28 -0700

    Add radio button flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, class_name: "Airport"
   belongs_to :to_airport, class_name: "Airport"
+
+  def to_s
+    "#{from_airport.airport_code} to #{to_airport.airport_code} @ #{start} (#{duration} minutes)"
+  end
 end
